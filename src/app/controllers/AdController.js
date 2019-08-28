664f2c078f019276bfe8bd53f2653c39218e878f
Harbs <harbsprog@gmail.com>
2019-08-27 23:08:52 -0300

    Api Rest pagination and its filters

--- src/app/controllers/AdController.js
+++ src/app/controllers/AdController.js
@@ -2,7 +2,33 @@ const Ad = require('../models/Ad')
 
 class AdController {
   async index (req, res) {
-    const ads = await Ad.find()
+    const filters = {}
+
+    if (req.query.price_min || req.query.price_max) {
+      filters.price = {}
+
+      // $gte é uma sigla para greather then(mongoose)
+      if (req.query.price_min) {
+        filters.price.$gte = req.query.price_min
+      }
+
+      // $gte é uma sigla para lower then(mongoose)
+      if (req.query.price_max) {
+        filters.price.$lte = req.query.price_max
+      }
+    }
+
+    if (req.query.title) {
+      filters.title = new RegExp(req.query.title, 'i')
+    }
+
+    // Faz uso do mongoose paginate
+    const ads = await Ad.paginate(filters, {
+      page: req.query.page || 1,
+      limit: 20,
+      populate: ['author'],
+      sort: '-createdAt'
+    })
 
     return res.json(ads)
   }
